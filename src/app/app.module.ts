39e364189f5d8755a38bd5211311d2e2897f552b
Gabriel Barbosa <gabrielbarbosaoliveira11999@gmail.com>
2023-03-18 22:04:35 -0300

    Ajustado tabela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { RadapeComponent } from './radape/radape.component';
 import { FormListComponent } from './list/form-list/form-list.component';
+import { TableListComponent } from './list/table-list/table-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
     RadapeComponent,
-    FormListComponent
+    FormListComponent,
+    TableListComponent
   ],
   imports: [
     BrowserModule
